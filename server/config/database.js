1250bc8a854aa7041e7d4e540ecc8c9e33ccf6f8
Adam Rogala <adam.rogala.ext@unit4.com>
2020-01-25 20:59:38 +0100

    change react-app into create-react-app

--- server/config/database.js
+++ server/config/database.js
@@ -4,6 +4,15 @@ const mongoose = require('mongoose');
 const dbRoute =
   `mongodb+srv://${databaseCredentials}@cluster0-lxxlf.mongodb.net/test?retryWrites=true&w=majority`;
 
-mongoose.connect(dbRoute, { useNewUrlParser: true });
+console.log('Trying to connect to the database with credentials:', databaseCredentials);
+
+mongoose
+  .connect(dbRoute, { useNewUrlParser: true })
+  .then(() => {
+    console.log('Connected to the database.');
+  })
+  .catch((error) => {
+    console.log('Connection to the database failed, error:', error);
+  })
 
 module.exports = mongoose.connection;
