9a4feade6059f70af640a8c79cebdf8bedf6f961
Adam Rogala <adam.rogala.ext@unit4.com>
2020-03-06 21:15:00 +0100

    fix marker styles

--- client/src/map/components/map/MapWrapper.tsx
+++ client/src/map/components/map/MapWrapper.tsx
@@ -3,8 +3,19 @@ import { Map, TileLayer, Marker, Popup } from 'react-leaflet';
 import './MapWrapper.css';
 import 'leaflet/dist/leaflet.css';
 
+const L = require("leaflet");
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png')
+});
 
 export default class MapWrapper extends React.Component {
+    
+
     state = {
         lat: 50.881066,
         lng: 15.713790,
@@ -14,14 +25,14 @@ export default class MapWrapper extends React.Component {
     render() {
         const position = { lat: this.state.lat, lng: this.state.lng };
         return <div className="map-wrapper">
-            <h2>Map</h2>
+            <h2>Wybierz zawody na mapie i sprawdź szczegóły</h2>
             <Map center={position} zoom={this.state.zoom}>
             <TileLayer
                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-            <Marker position={position}>
+            <Marker position={position} draggable={true}>
                 <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
+                    Tu mieszka Julek.
                 </Popup>
             </Marker>
             </Map>
