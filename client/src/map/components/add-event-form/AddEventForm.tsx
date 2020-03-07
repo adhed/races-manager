67fed31f79db0f751447d2355848808e85015d5b
Adam Rogala <adam.rogala.ext@unit4.com>
2020-03-07 20:09:54 +0100

    add new styles

--- client/src/map/components/add-event-form/AddEventForm.tsx
+++ client/src/map/components/add-event-form/AddEventForm.tsx
@@ -6,36 +6,44 @@ export default class AddEventForm extends React.Component {
         return <div className="add-event-form">
             <form className="add-event-form__form form">
                 <div className="form__element">
-                    <label className="form__label" htmlFor="name">Nazwa zawodów</label>
-                    <input name="name" type="text" id="name" placeholder="Bikemaraton"></input>
+                    <label className="form__label" htmlFor="name">Nazwa</label>
+                    <input name="name" type="text" id="name" placeholder="Muflon MTB"></input>
+                </div>
+                <div className="form__box">
+                    <div className="form__element">
+                        <label className="form__label" htmlFor="name">Seria</label>
+                        <input name="series" type="text" id="series" placeholder="Strefa MTB Sudety"></input>
+                    </div>
+                    <div className="form__element">
+                        <label className="form__label" htmlFor="date">Data</label>
+                        <input name="date" type="date" id="date" placeholder="01.05.2020"></input>
+                    </div>
                 </div>
                 
-                <div className="form__element">
-                    <label className="form__label" htmlFor="date">Data</label>
-                    <input name="date" type="date" id="date" placeholder="01.05.2020"></input>
+                <div className="form__box">
+                    <div className="form__element">
+                        <label className="form__label" htmlFor="date">Dyscyplina</label>
+                        <input name="date" type="text"></input>
+                    </div>
+                    <div className="form__element">
+                        <label className="form__label" htmlFor="type">Typ</label>
+                        <input name="type" id="type" placeholder="XCM"></input>
+                    </div>
                 </div>
                 <div className="form__element">
-                    <label className="form__label" htmlFor="date">Dyscyplina</label>
-                    <input name="date" type="text"></input>
-                </div>
-                <div className="form__element">
-                    <label className="form__label" htmlFor="place">Miejsce</label>
+                    <label className="form__label" htmlFor="place">Miejsce startu</label>
                     <input name="place" id="place" placeholder="Wałbrzych"></input>
                 </div>
                 <div className="form__element">
-                    <label className="form__label" htmlFor="type">Typ zawodów</label>
-                    <input name="type" id="type" placeholder="XCM"></input>
-                </div>
-                <div className="form__element">
-                    <label className="form__label" htmlFor="link">Link</label>
+                    <label className="form__label" htmlFor="link">Link do szczegółów</label>
                     <input name="link" id="link" placeholder="http://"></input>
                 </div>
                 <div className="form__element">
                     <label className="form__label" htmlFor="description">Opis</label>
-                    <textarea name="description" id="description" rows={4} placeholder=""></textarea>
+                    <textarea name="description" id="description" rows={6} placeholder="Mocne MTB na rozpoczęcie sezonu, zapraszamy!"></textarea>
                 </div>
                 <button className="form__button button">
-                    <span className="button__label">Zgłoś zawody</span>
+                    <span className="button__label">Dodaj zawody</span>
                 </button>
             </form>
         </div>
