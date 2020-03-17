6457a7a5c0398262b18965d6238eed400e115b4d
Adam Rogala <a.rogala@nowaera.pl>
2020-03-17 22:10:52 +0100

    add form validation

--- client/src/map/components/add-event-form/AddEventForm.tsx
+++ client/src/map/components/add-event-form/AddEventForm.tsx
@@ -1,36 +1,47 @@
-import React, { FormEvent } from 'react';
+import React, { useState } from 'react';
 import Geosuggest, { Suggest } from 'react-geosuggest';
-import { useForm } from 'react-hook-form'
+import { useForm, Controller } from 'react-hook-form'
 
 import './AddEventForm.scss';
 import { SportEvent } from '../../../shared/models/sport-event';
 import { MarkerCoordinates } from '../../../shared/models/map';
 import { getCurrentDate } from '../../../shared/utils';
+import { Discipline, DISCPLINES_NAMES, DISCIPLINES_TYPES, DISCIPLINES_TYPES_NAMES } from '../../../shared/models/disciplines';
 
 type AddEventFormProps = {
     onSuggetionChange: (coords: MarkerCoordinates) => void,
     onFormSubmit: (sportEvent: SportEvent) => void,
 }
 
+const disciplines: Discipline[] = [Discipline.MountainBiking, Discipline.RoadCycling, Discipline.Running, Discipline.XcSkiing];
+
 export default function AddEventForm(props: AddEventFormProps) {
-    const { register, handleSubmit, watch, errors } = useForm();
+    const { control, setValue, register, handleSubmit, errors } = useForm();
+    const [selectedDiscipline, setSelectedDiscipline] = useState(Discipline.MountainBiking);
     const defaultDate = getCurrentDate();
 
     const onSubmit = (data: any): void => {
-        console.log('data', data);
-        // const formData = new FormData(event.target as any);
+        console.log('Submitted data:', data);
         
-        // props.onFormSubmit({
-        //     name: '',
-        //     serie: String(formData.get('serie')),
-        //     date: new Date(String(formData.get('date'))),
-        //     discipline: String(formData.get('discipline')),
-        //     place: String(formData.get('place')),
-        //     link: String(formData.get('link')),
-        //     type: String(formData.get('type')),
-        //     description: String(formData.get('description')),
-        //     coordinates: { lat: 0, lng: 0 }
-        // });
+        props.onFormSubmit({
+            name: data.name,
+            serie: data.serie,
+            date: new Date(data.date),
+            discipline: data.discipline,
+            place: data.place,
+            link: data.link,
+            type: data.type,
+            description: data.discription,
+            coordinates: { lat: 0, lng: 0 }
+        });
+    }
+
+    const handleDisciplineChange = (event: any): void => {
+        if (!event.target.value) {
+            return;
+        }
+
+        setSelectedDiscipline(event.target.value as Discipline);
     }
 
     const handleSuggestionSelect = (suggestion: Suggest): void => {
@@ -38,23 +49,17 @@ export default function AddEventForm(props: AddEventFormProps) {
             return;
         }
     
+        setValue('place', suggestion.label);
         props.onSuggetionChange({
             lat: suggestion.location.lat,
             lng: suggestion.location.lng
         })
     }
 
-    const handleOnNameChange = (name: string): void => {
-        if (!name) {
-            return;
-        }
-    }
-
-
     return <div className="add-event-form">
         <form className="add-event-form__form form" id="addEventForm" name="addEventForm" onSubmit={handleSubmit(onSubmit)}>
-            <div className="form__element">
-                <label className="form__label" htmlFor="name">Nazwa</label>
+            <div className={errors.name ? 'form__element form__element--error' : "form__element"}>
+                <label className="form__label" htmlFor="name">Nazwa*</label>
                 <input name="name" type="text" id="name" placeholder="Muflon MTB" ref={register({ required: true })} />
             </div>
             <div className="form__box">
@@ -63,29 +68,42 @@ export default function AddEventForm(props: AddEventFormProps) {
                     <input name="serie" type="text" id="serie" placeholder="Strefa MTB Sudety" ref={register} />
                 </div>
                 <div className="form__element">
-                    <label className="form__label" htmlFor="date">Data</label>
+                    <label className="form__label" htmlFor="date">Data*</label>
                     <input name="date" type="date" id="date" placeholder="01-06-2020" required defaultValue={defaultDate} ref={register({ required: true })} />
                 </div>
             </div>
             
             <div className="form__box">
                 <div className="form__element">
-                    <label className="form__label" htmlFor="discipline">Dyscyplina</label>
-                    <select name="discipline" id="discipline" ref={register({ required: true })}>
-                        <option>Kolarstwo górskie</option>
-                        <option>Bieganie</option>
-                        <option>Narciarstwo biegowe</option>
-                        <option>Kolarstwo szosowe</option>
+                    <label className="form__label" htmlFor="discipline">Dyscyplina*</label>
+                    <select name="discipline" id="discipline" onChange={handleDisciplineChange} ref={register({ required: true })}>
+
+                        { disciplines.map((discipline: Discipline) => {
+                            return (<option key={discipline}  value={discipline}>{DISCPLINES_NAMES[discipline]}</option>)
+                        }) }
                     </select>
                 </div>
                 <div className="form__element">
                     <label className="form__label" htmlFor="type">Typ</label>
-                    <input name="type" id="type" type="text" placeholder="XCM" ref={register} />
+                    <select name="type" id="type" ref={register({ required: false })}>
+                        { (DISCIPLINES_TYPES[selectedDiscipline] as string[]).map((type: string) => {
+                            return (<option key={`${type}${selectedDiscipline}`} value={type}>{DISCIPLINES_TYPES_NAMES[type]}</option>)
+                        }) }
+                    </select>
                 </div>
             </div>
-            <div className="form__element">
-                <label className="form__label" htmlFor="place">Miejsce startu</label>
-                <Geosuggest placeholder="Wpisz miejsce startu" onChange={handleOnNameChange} onSuggestSelect={handleSuggestionSelect} />
+            <div className={errors.place ? 'form__element form__element--error' : "form__element"}>
+                <label className="form__label" htmlFor="place">Miejsce startu*</label>
+                <Controller
+                    as={Geosuggest}
+                    name="place"
+                    control={control}
+                    rules={{ required: true }}
+                    onChange={([place]) => place}
+                    onSuggestSelect={handleSuggestionSelect}
+                    defaultValue={''}
+                />
+                {/* <Geosuggest placeholder="Wpisz i wyszukaj miejsce startu" onChange={handleOnNameChange} onSuggestSelect={handleSuggestionSelect} /> */}
             </div>
             <div className="form__element">
                 <label className="form__label" htmlFor="link">Link do szczegółów</label>
--- client/src/shared/models/disciplines.ts
+++ client/src/shared/models/disciplines.ts
@@ -1,39 +1,71 @@
 export enum Discipline {
-    RoadCycling,
-    MountainBiking,
-    Running,
-    XcSkiing,
+    RoadCycling = 'road-cycling',
+    MountainBiking = 'mtb',
+    Running = 'running',
+    XcSkiing = 'xcskiing',
 }
 
 export enum MountainBikeEventType {
-    XCM,
-    XCO,
-    XCE,
-    StageRace,
+    XCM = 'xcm',
+    XCO = 'xco',
+    XCE = 'xce',
+    StageRace = 'mtb-stage-race',
 }
 
 export enum RunningEventType {
-    Marathon,
-    Halfmarathon,
-    StreetRace,
-    Ultramarathon,
-    CrossRace,
+    Marathon = 'run-marathon',
+    Halfmarathon = 'halfmarathon',
+    StreetRace = 'run-stage-race',
+    Ultramarathon = 'run-ultra-marathon',
+    CrossRace = 'cross-race',
 }
 
 export enum RoadCyclingEventType {
-    TimeTrial,
-    StageRace,
-    SingleDay,
+    TimeTrial = 'road-time-trial',
+    StageRace = 'road-stage-race',
+    SingleDay = 'road-single-day',
+}
+
+export enum CrossCountrySkiingEventType {
+    TimeTrial = 'xc-time-trial',
+    MassStart = 'xc-mass-start',
+    Marathon = 'xc-marathon',
+    Ultramarathon = 'xc-ultra-marathon',
 }
 
 export interface KeyLabel {
     [key: string]: string;
 }
 
-
-export const DISCPLINES: KeyLabel = {
+export const DISCPLINES_NAMES: KeyLabel = {
     [Discipline.MountainBiking]: 'Kolarstwo górskie',
     [Discipline.RoadCycling]: 'Kolarstwo szosowe',
     [Discipline.Running]: 'Bieganie',
     [Discipline.XcSkiing]: 'Narciarstwo biegowe',
+}
+
+export const DISCIPLINES_TYPES_NAMES: KeyLabel = {
+    [MountainBikeEventType.XCM]: 'Maraton',
+    [MountainBikeEventType.XCO]: 'XCO',
+    [MountainBikeEventType.XCE]: 'Eliminator',
+    [MountainBikeEventType.StageRace]: 'Wyścig etapowy',
+    [RoadCyclingEventType.SingleDay]: 'Wyścig jednodniowy',
+    [RoadCyclingEventType.TimeTrial]: 'Czasówka',
+    [RoadCyclingEventType.StageRace]: 'Wyścig etapowy',
+    [RunningEventType.CrossRace]: 'Cross',
+    [RunningEventType.Halfmarathon]: 'Półmaraton',
+    [RunningEventType.Marathon]: 'Maraton',
+    [RunningEventType.Ultramarathon]: 'Ultramaraton',
+    [RunningEventType.StreetRace]: 'Wyścig miejski',
+    [CrossCountrySkiingEventType.TimeTrial]: 'Czasówka',
+    [CrossCountrySkiingEventType.Marathon]: 'Maraton',
+    [CrossCountrySkiingEventType.MassStart]: 'Inny wyścig ze startu wspólnego',
+    [CrossCountrySkiingEventType.Ultramarathon]: 'Ultramaraton',
+}
+
+export const DISCIPLINES_TYPES = {
+    [Discipline.MountainBiking]: [MountainBikeEventType.XCM, MountainBikeEventType.XCO, MountainBikeEventType.XCE, MountainBikeEventType.StageRace],
+    [Discipline.RoadCycling]: [RoadCyclingEventType.SingleDay, RoadCyclingEventType.StageRace, RoadCyclingEventType.TimeTrial],
+    [Discipline.Running]: [RunningEventType.Halfmarathon, RunningEventType.Marathon, RunningEventType.Ultramarathon, RunningEventType.StreetRace, RunningEventType.CrossRace],
+    [Discipline.XcSkiing]: [CrossCountrySkiingEventType.Marathon, CrossCountrySkiingEventType.Ultramarathon, CrossCountrySkiingEventType.TimeTrial,  CrossCountrySkiingEventType.MassStart],
 }
