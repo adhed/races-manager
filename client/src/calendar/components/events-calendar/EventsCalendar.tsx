ec628fd5639eb39c72f22160c0dfb7a593480531
Adam Rogala <adamroweb@gmail.com>
2020-03-22 15:21:41 +0100

    fetch events when it was not done before

--- client/src/calendar/components/events-calendar/EventsCalendar.tsx
+++ client/src/calendar/components/events-calendar/EventsCalendar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Event, Calendar, momentLocalizer,  } from 'react-big-calendar'
 import { connect } from 'react-redux';
 import moment from 'moment'
@@ -7,16 +7,23 @@ import 'react-big-calendar/lib/css/react-big-calendar.css';
 import './EventsCalendar.scss';
 import { ApplicationState } from '../../../state/ducks';
 import { mapSportEventsToCalendar } from '../../../shared/utils/sport-event.utils';
-import { selectEventById } from '../../../state/ducks/sport-event/actions';
+import { selectEventById, fetchSportEvents } from '../../../state/ducks/sport-event/actions';
 
 const localizer = momentLocalizer(moment);
 
 type EventsCalendarProps = {
-    events?: Event[];
-    selectEventById: (id: string) => void; 
+    events: Event[];
+    selectEventById: (id: string) => void;
+    fetchSportEvents: () => void;
 }
 
 function EventsCalendar(props: EventsCalendarProps): JSX.Element {
+    useEffect(() => {
+        if (!props.events.length) {
+            props.fetchSportEvents();
+        }
+    });
+
     const handleSelectEvent = (event: any): void => {
         props.selectEventById(event.id);
     }
@@ -40,4 +47,4 @@ function mapStateToProps(state: ApplicationState) {
         events: mapSportEventsToCalendar(sportEvent.data),
     };
 }
-export default connect(mapStateToProps, { selectEventById })(EventsCalendar);
+export default connect(mapStateToProps, { selectEventById, fetchSportEvents })(EventsCalendar);
