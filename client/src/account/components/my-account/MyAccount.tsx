ed0649e57e630ff163d5e496fb18c6bdd216087d
Adam Rogala <adamroweb@gmail.com>
2020-05-11 21:56:03 +0200

    add admin icon

--- client/src/account/components/my-account/MyAccount.tsx
+++ client/src/account/components/my-account/MyAccount.tsx
@@ -1,19 +1,27 @@
 import React from 'react';
 import { UserInfo } from 'firebase';
 import { connect } from 'react-redux';
+import { faUser, faUserShield } from '@fortawesome/free-solid-svg-icons';
 
-import './MyAccount.css';
 import { ApplicationState } from '../../../state/ducks';
 import FavouriteEvents from '../favourite-events/FavouriteEvents';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import './MyAccount.scss';
 
 type MyAccountProps = {
     isLoggedIn: boolean;
     user: UserInfo | null;
+    isAdmin: boolean;
 }
 
 function MyAccount(props: MyAccountProps) {
-    return <div>
-        <h2>Witaj { props.user ? props.user.displayName || 'Unknown' : 'Unknown' }</h2>
+    const icon = props.isAdmin ? faUserShield : faUser;
+
+    return <div className="my-account-wrapper">
+        <h2>
+            <FontAwesomeIcon className="my-account-wrapper__icon" icon={icon} />
+            Witaj { props.user ? props.user.displayName || 'Unknown' : 'Unknown' }
+        </h2>
         <FavouriteEvents />
     </div>;
 }
@@ -22,6 +30,7 @@ const mapStateToProps = (state: ApplicationState) => {
     return {
         isLoggedIn: state.account.isLoggedIn,
         user: state.account.user,
+        isAdmin: state.account.details.isAdmin,
     };
 }
   
