d26dfbf63fa9a0187fe1e55c149c5c7f53d67c7a
Adam Rogala <adamroweb@gmail.com>
2020-05-18 14:12:20 +0200

    add credentials

---
+++ client/src/config/api-credentials.js
@@ -0,0 +1,1 @@
+export const MAP_API_KEY = 'AIzaSyAOSXLe-sObJrkOQliLs6F5RErMw2vq64I';
---
+++ client/src/config/firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyAOSXLe-sObJrkOQliLs6F5RErMw2vq64I",
+    authDomain: "races-manager.firebaseapp.com",
+    databaseURL: "https://races-manager.firebaseio.com",
+    projectId: "races-manager",
+    storageBucket: "races-manager.appspot.com",
+    messagingSenderId: "840684176512",
+    appId: "1:840684176512:web:bfdc19854eddebb1fad265"
+};
