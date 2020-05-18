163987c07ff078fca42fbf92a61aedcb6040effa
Adam Rogala <adamroweb@gmail.com>
2020-05-18 23:45:58 +0200

    change port

--- server/app.js
+++ server/app.js
@@ -7,7 +7,6 @@ const logger = require('morgan');
 const cors = require('cors');
 const apiRouter = require('./routes/api.router');
 
-const API_PORT = 3001;
 const app = express();
 
 app.use(cors());
@@ -43,6 +42,6 @@ app.use(function(err, req, res, next) {
 });
 
 // launch our backend into a port
-app.listen(API_PORT, () => console.log(`LISTENING ON PORT ${API_PORT}`));
+app.listen(process.env.PORT || 3001, () => console.log(`LISTENING ON PORT ${API_PORT}`));
 
 module.exports = app;
